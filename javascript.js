616abb754d80b127858e8d48e0d1b43076a629df
vmferreira <vmferreira@inf.ufsm.br>
2026-08-24 17:44:51 -0300

    commit

--- javascript.js
+++ javascript.js
@@ -5,7 +5,8 @@ const viewport = document.getElementById("viewport");
 const viewportItems = document.getElementById("viewport-items");
 const annotationsContainer = document.getElementById("annotations-container");
 
-const infoBoxes = {
+const properties = {
+  div: document.getElementById("properties-box"),
   x: document.getElementById("note-x").getElementsByClassName("info")[0],
   y: document.getElementById("note-y").getElementsByClassName("info")[0],
   w: document.getElementById("note-w").getElementsByClassName("info")[0],
@@ -13,24 +14,53 @@ const infoBoxes = {
   color: document.getElementById("color-input"),
   alpha: document.getElementById("opacity-input"),
   tag: document.getElementById("tag-input"),
+  fontSize: document.getElementById("tag-font-size-input"),
+  circle: document.getElementById("tag-circular-input"),
+}
+const buttons = {
+  select: document.getElementById("select"),
+  draw: document.getElementById("draw"),
+  delete: document.getElementById("delete"),
+  zoomOut: document.getElementById("zoom-out"),
+  zoomIn: document.getElementById("zoom-in"),
+  zoomDisplay: document.getElementById("zoom-display"),
+  remove: document.getElementById("remove"),
+}
+
+let mode = "select"
+
+let cam = { x: 0, y: 0, scale: 1 };
+
+
+function camZoom(amt) {
+  camSetZoom(cam.scale + amt);
+}
+
+function camSetZoom(amt) {
+  cam.scale = amt;
+  if (cam.scale < 0.01) cam.scale = 0.01;
+  viewportItems.style.scale = cam.scale;
+  buttons.zoomDisplay.innerText = Math.round(cam.scale * 100) + "%";
 }
 
 
-let cam = { x: 0, y: 0, scale: 1 };
 let selectedNote = null;
 let selectedHandle = null;
 let imgWidth = 0;
 let imgHeight = 0;
-images = [];
+let images = [];
+let imageIndex = -1;
 
 function newAnnotation() {
   return {
     x: Math.random()*64,
     y: Math.random()*64,
-    w: 96, 
-    h: 32,
+    w: 8, 
+    h: 8,
     text: "Your Note Here",
+    fontSize: 12,
     col: {r: Math.random()*256, g: Math.random()*256, b: Math.random()*256, a: 0.5},
+    circle: false,
   };
 }
 
@@ -62,12 +92,14 @@ function annotationElement(annotation) {
   box.style.minheight = annotation.h + "px";
   box.className = "box";
   note.className = "note-text";
+  note.style.fontSize = annotation.fontSize;
   let colString = annotationColorString(annotation);
   box.style.backgroundColor = colString;
   note.style.backgroundColor = colString;
   div.appendChild(box);
   div.appendChild(note);
   div.annotation = annotation;
+  annotation.elem = div;
 
   let handlePositions = [
     {top: "0px", left: "0px", point: "nw"},
@@ -86,19 +118,22 @@ function annotationElement(annotation) {
     div.appendChild(handle);
   }
   updatePropertiesPanel(annotation);
+  updateAnnotationElement(div);
   return div;
 }
 
 function updatePropertiesPanel(annotation) {
   let colString = annotationColorStringHex(annotation);
   let alphaString = Math.round(annotation.col.a * 100).toString();
-  infoBoxes.x.innerText = Math.round(annotation.x);
-  infoBoxes.y.innerText = Math.round(annotation.y);
-  infoBoxes.w.innerText = Math.round(annotation.w);
-  infoBoxes.h.innerText = Math.round(annotation.h);
-  infoBoxes.color.value = colString;
-  infoBoxes.alpha.value = alphaString;
-  infoBoxes.tag.value = annotation.text;
+  properties.x.innerText = Math.round(annotation.x);
+  properties.y.innerText = Math.round(annotation.y);
+  properties.w.innerText = Math.round(annotation.w);
+  properties.h.innerText = Math.round(annotation.h);
+  properties.color.value = colString;
+  properties.alpha.value = alphaString;
+  properties.tag.value = annotation.text;
+  properties.fontSize.value = annotation.fontSize;
+  properties.circle.checked = annotation.circle;
 }
 
 function updateAnnotationElement(elem) {
@@ -118,6 +153,13 @@ function updateAnnotationElement(elem) {
   let colString = annotationColorString(annotation);
   box.style.backgroundColor = colString;
   note.style.backgroundColor = colString;
+  note.style.fontSize = annotation.fontSize + "px";
+  note.style.top = "-" + (annotation.fontSize * 2 + 9) + "px";
+  if (annotation.circle) {
+    box.style.borderRadius = "50%";
+  } else {
+    box.style.borderRadius = "5px";
+  }
 
   let handlePositions = [
     {top: "0px", left: "0px", point: "nw"},
@@ -138,59 +180,67 @@ function updateAnnotationElement(elem) {
   updatePropertiesPanel(annotation);
 }
 
-function setMainImage(index) {
-  mainImage.src = images[index].url;
-  cam = { x: 0, y: 0, scale: 1 };
-  console.log(mainImage.width + ", " + mainImage.height);
-  imgWidth = mainImage.width;
-  imgHeight = mainImage.height;
-  cam.x = viewport.clientWidth / 2 - mainImage.width / 2
-  cam.y = viewport.clientHeight / 2 - mainImage.height / 2
-  viewportItems.style.translate = cam.x + "px " + cam.y +"px";
-  viewportItems.style.scale = cam.scale;
-  mainImage.style.display = "flow";
-
+function regenerateAnnotations() {
   while (annotationsContainer.firstChild) {
     annotationsContainer.removeChild(annotationsContainer.firstChild);
   }
-
-  for (const annotation of images[index].annotations) {
-    annotationsContainer.appendChild(annotationElement(annotation));
-    console.log(annotation);
+  if (imageIndex > -1) {
+    for (const annotation of images[imageIndex].annotations) {
+      annotationsContainer.appendChild(annotationElement(annotation));
+    }
+    let preview = imageList.getElementsByClassName("image-preview")[imageIndex];
+    let noteCount = preview.getElementsByClassName("annotation-count")[0];
+    noteCount.innerText = images[imageIndex].annotations.length + " Anotações";
   }
-  console.log(cam);
+}
+
+function setMainImage(index) {
+  imageIndex = index;
+  selectedNote = null;
+  selectedHandle = null;
+  if (index == -1) {
+    mainImage.style.display = "none";
+    while (annotationsContainer.firstChild) {
+      annotationsContainer.removeChild(annotationsContainer.firstChild);
+    }
+  } else {
+    mainImage.src = images[index].url;
+    cam = { x: 0, y: 0, scale: 1 };
+    imgWidth = mainImage.width;
+    imgHeight = mainImage.height;
+    cam.x = viewport.clientWidth / 2 - mainImage.width / 2
+    cam.y = viewport.clientHeight / 2 - mainImage.height / 2
+    viewportItems.style.translate = cam.x + "px " + cam.y +"px";
+    viewportItems.style.scale = cam.scale;
+    mainImage.style.display = "flow";
+    regenerateAnnotations();
+  }
+
 }
 
 function createImagePreview(image, num) {
-    const imgElement  = document.createElement("img"); 
-    const previewElement = document.createElement("div");
-    const nameElement = document.createElement("p");
-    const noteCount = document.createElement("p");
-    const div = document.createElement("div");
-    noteCount.innerText = "0 Anotações";
-    nameElement.innerText = image.name;
-    noteCount.className = "annotation-count";
-    nameElement.className = "image-name";
-    imgElement.src = image.url; 
-    previewElement.imgNumber = num;
-    previewElement.className = "image-preview";
-    imgElement.className = "image-preview-image";
-    previewElement.appendChild(imgElement);
-    previewElement.appendChild(div);
-    div.appendChild(nameElement);
-    div.appendChild(noteCount);
-
-    return previewElement;
+  const imgElement  = document.createElement("img"); 
+  const previewElement = document.createElement("div");
+  const nameElement = document.createElement("p");
+  const noteCount = document.createElement("p");
+  const div = document.createElement("div");
+  noteCount.innerText = image.annotations.length + " Anotações";
+  nameElement.innerText = image.name;
+  noteCount.className = "annotation-count";
+  nameElement.className = "image-name";
+  imgElement.src = image.url; 
+  previewElement.imgNumber = num;
+  previewElement.className = "image-preview";
+  imgElement.className = "image-preview-image";
+  previewElement.appendChild(imgElement);
+  previewElement.appendChild(div);
+  div.appendChild(nameElement);
+  div.appendChild(noteCount);
+  
+  return previewElement;
 }
 
-function onFilePick() {
-  for (const img of filePicker.files) {
-    images.push({ 
-      name: img.name, 
-      url: URL.createObjectURL(img), 
-      annotations: [newAnnotation(),newAnnotation(),newAnnotation(),]
-    }); 
-  }
+function updateImageList() {
   while (imageList.firstChild) {
     imageList.removeChild(imageList.firstChild);
   }
@@ -203,6 +253,17 @@ function onFilePick() {
     imageList.appendChild(newImage);
     i += 1;
   }
+}
+
+function onFilePick() {
+  for (const img of filePicker.files) {
+    images.push({ 
+      name: img.name, 
+      url: URL.createObjectURL(img), 
+      annotations: []
+    }); 
+  }
+  updateImageList();
 }
 filePicker.addEventListener("input", onFilePick);
 
@@ -244,11 +305,11 @@ viewport.addEventListener("pointermove", function(e) {
       selectedNote.annotation.y += y;
       updateAnnotationElement(selectedNote);
       } else {
-      cam.x += x;
-      cam.y += y;
-      viewportItems.style.translate = cam.x + "px " + cam.y +"px";
+        cam.x += x;
+        cam.y += y;
+        viewportItems.style.translate = cam.x + "px " + cam.y +"px";
+      }
     }
-  }
 }, true);
 
 viewport.addEventListener("pointerdown", function(e) {
@@ -256,21 +317,77 @@ viewport.addEventListener("pointerdown", function(e) {
   let handle = e.target.closest(".handle")
   if (handle) {
     selectedHandle = handle;
+    selectedNote = selectedHandle.note;
+    properties.div.style.visibility = "visible";
     updatePropertiesPanel(selectedHandle.note.annotation);
   } else  if (note) {
     selectedHandle= null;
     selectedNote = note;
+    properties.div.style.visibility = "visible";
     updatePropertiesPanel(selectedNote.annotation);
   } else {
-    selectedHandle= null;
-    selectedNote = null;
+    if (mode == "select") {
+      properties.div.style.visibility = "hidden";
+      selectedHandle= null;
+      selectedNote = null;
+    } else {
+      const viewportRect = this.getBoundingClientRect();
+      let n = newAnnotation();
+      n.x = (e.clientX - viewportRect.left - cam.x)/ cam.scale - n.w;
+      n.y = (e.clientY - viewportRect.top - cam.y)/ cam.scale - n.h;
+      images[imageIndex].annotations.push(n);
+      regenerateAnnotations();
+      selectedHandle = n.elem.getElementsByClassName("handle")[3];
+    }
   }
 }, true);
 
+viewport.addEventListener("drop", function(e) {
+  e.preventDefault();
+  for (const img of e.dataTransfer.files) {
+    images.push({ 
+      name: img.name, 
+      url: URL.createObjectURL(img), 
+      annotations: [newAnnotation()]
+    }); 
+  }
+  updateImageList();
+});
+
+viewport.addEventListener("dragover", function(e) {
+  const fileItems = [...e.dataTransfer.items].filter(
+    (item) => item.kind === "file",
+  );
+  if (fileItems.length > 0) {
+    e.preventDefault();
+    if (fileItems.some((item) => item.type.startsWith("image/"))) {
+      e.dataTransfer.dropEffect = "copy";
+    } else {
+      e.dataTransfer.dropEffect = "none";
+    }
+  }
+});
+
+window.addEventListener("dragover", (e) => {
+  const fileItems = [...e.dataTransfer.items].filter(
+    (item) => item.kind === "file",
+    );
+  if (fileItems.length > 0) {
+    e.preventDefault();
+    if (!viewport.contains(e.target)) {
+      e.dataTransfer.dropEffect = "none";
+    }
+  }
+});
+
+window.addEventListener("drop", (e) => {
+  if ([...e.dataTransfer.items].some((item) => item.kind === "file")) {
+    e.preventDefault();
+  }
+});
+
 viewport.addEventListener("wheel", function(e) {
-  cam.scale += e.deltaY / 100.0;
-  if (cam.scale < 0.01) cam.scale = 0.01;
-  viewportItems.style.scale = cam.scale;
+  camZoom(e.deltaY / 100.0);
 });
 mainImage.style.display = "none";
 
@@ -282,7 +399,7 @@ function parseColor(hex) {
   return color;
 }
 
-infoBoxes.color.addEventListener("input", function(e) {
+properties.color.addEventListener("input", function(e) {
   if (selectedNote) {
     let color = parseColor(this.value);
     selectedNote.annotation.col.r = color.r;
@@ -292,14 +409,66 @@ infoBoxes.color.addEventListener("input", function(e) {
   }});
 
 
-infoBoxes.alpha.addEventListener("input", function(e) {
+properties.alpha.addEventListener("input", function(e) {
   if (selectedNote) {
     selectedNote.annotation.col.a = parseInt(this.value) / 100;
     updateAnnotationElement(selectedNote);
   }});
 
-infoBoxes.tag.addEventListener("input", function(e) {
+properties.tag.addEventListener("input", function(e) {
     if (selectedNote) {
     selectedNote.annotation.text = this.value;
     updateAnnotationElement(selectedNote);
   }});
+properties.fontSize.addEventListener("input", function(e) {
+    if (selectedNote) {
+    selectedNote.annotation.fontSize = parseInt(this.value);
+    updateAnnotationElement(selectedNote);
+  }});
+properties.circle.addEventListener("input", function(e) {
+    if (selectedNote) {
+    selectedNote.annotation.circle = this.checked;
+    updateAnnotationElement(selectedNote);
+  }});
+
+buttons.select.addEventListener("click", function(e) {
+  this.className = "tool-button current";
+  buttons.draw.className = "tool-button";
+  mode = "select";
+});
+buttons.draw.addEventListener("click", function(e) {
+  this.className = "tool-button current";
+  buttons.select.className = "tool-button";
+  mode = "draw";
+});
+buttons.zoomOut.addEventListener("click", function(e) {
+  camZoom(-5/100.0);
+});
+buttons.zoomIn.addEventListener("click", function(e) {
+  camZoom(5/100.0);
+});
+buttons.zoomDisplay.addEventListener("click", function(e) {
+  camSetZoom(1.0);
+});
+buttons.delete.addEventListener("click", function(e) {
+  if (selectedNote) {
+    images[imageIndex].annotations.splice(images[imageIndex].annotations.indexOf(selectedNote.annotation), 1);
+    regenerateAnnotations();
+    selectedNote = null;
+    properties.div.style.visibility = "hidden";
+  }
+});
+buttons.remove.addEventListener("click", function(e) {
+  if (imageIndex == -1) return;
+  images.splice(imageIndex, 1);
+  if (images.length == 0)  {
+    setMainImage(-1);
+  } else {
+    while (imageIndex >= images.length) {
+      imageIndex--;
+    }
+    setMainImage(imageIndex);
+  }
+  updateImageList();
+});
+properties.div.style.visibility = "hidden";
